4c30dd445376712ef870da984c3eba6c9e806e9f
Wanjie-Ryan <ryanwanjiedistortion@gmail.com>
2024-09-26 10:21:19 +0300

    hotel app

---
+++ src/components/header.vue
@@ -0,0 +1,9 @@
+<template>
+
+<h2>Header</h2>
+
+</template>
+
+<script>
+
+</script>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,10 +1,19 @@
 <template>
-  <p>Home</p>
+  <div>
+    <Header />
+
+    <p>Home</p>
+  </div>
 </template>
 
 <script>
+import Header from "./header.vue";
+
 export default {
   name: "Home",
+  components: {
+    Header,
+  },
   mounted() {
     let loggedUser = localStorage.getItem("UserInfo");
     console.log(loggedUser);
